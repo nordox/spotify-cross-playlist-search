da934a5c3e49cc86cf3183b263005cfeb5e51174
Dox <ngk5036@gmail.com>
2023-04-26 20:41:51 -0400

    rework styles for not available tracks

--- static/main.js
+++ static/main.js
@@ -69,7 +69,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 function setupClickHandlers() {
-    document.querySelectorAll(".open-track-btn > button").forEach(el => {
+    document.querySelectorAll(".open-track-btn > button.avail").forEach(el => {
         el.addEventListener('click', function (event) {
             event.preventDefault();
             const hasApp = $(".toggle").is(':checked');
@@ -111,16 +111,21 @@ function renderFound(found) {
             }
             const imgSrc = track.album.images[2]?.url;
             item.append(`
-                <div class="track ${!imgSrc ? "not-avail" : ""} ${!track.id ? "disabled" : ""}">
+                <div class="track ${!imgSrc ? "not-avail" : ""}">
                 <div class="album-cover">
-                    <img src="${imgSrc || "https://i.scdn.co/image/ab6775700000ee85aeb6fb34fde89e0c758f7bbb"}" alt="" width=64 height=64 />
-                    <svg role="img" height="16" width="16" viewBox="0 0 16 16" class="Svg-ytk21e-0 jAKAlG"><path d="M10 2v9.5a2.75 2.75 0 11-2.75-2.75H8.5V2H10zm-1.5 8.25H7.25A1.25 1.25 0 108.5 11.5v-1.25z"></path></svg>
+                    ${imgSrc
+                    ? `<img src="${imgSrc}" alt="" />`
+                    : `<div style="width:64px;height:64px">&nbsp;</div>`}
                 </div>
                 <div class="track-content">
                     <span class="track-name">${track.name}</span>
                     <span class="artists">${artists.join(', ')} · ${track.album.name}</span>
                 </div>
-                <div class="open-track-btn"><button class="open-spotify" data-href="${track.external_urls.spotify}" data-uri="${track.uri}"><img src="Spotify_Icon_RGB_White.png" alt="" />Play on Spotify</button></div>
+                <div class="open-track-btn">
+                    ${track.id
+                    ? `<button class="open-spotify avail" data-href="${track.external_urls.spotify}" data-uri="${track.uri}"><img src="Spotify_Icon_RGB_White.png" alt="" />Play on Spotify</button>`
+                    : `<button class="open-spotify not-avail"><img src="Spotify_Icon_RGB_White.png" alt="" /><span style="position:relative"><span>Play on Spotify</span><div>Not available</div></span></button>`}
+                </div>
                 </div>
             `);
         }
